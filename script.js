04e52bcf718d662191e5dc3a1ee076584b66130c
Kian Davey <kiandaveyantolin@gmail.com>
2026-08-28 20:55:16 +0800

    feat: added js functionalities

--- script.js
+++ script.js
@@ -4,17 +4,111 @@ const overlayBlur = document.getElementById("overlay");
 const closeBtn = document.getElementById("close-button");
 const cartBtn = document.getElementById("cart-button");
 const cartPopup = document.getElementById("cart-popup");
+const minusButton = document.getElementById("minus-button");
+const addButton = document.getElementById("plus-button");
+const amountSpan = document.getElementById("amount");
+const addToCartBtn = document.getElementById("add-to-cart-button");
+const numberItemsCart = document.getElementById("number-items-cart");
+const cartEmptyDiv = document.getElementById("cart-empty");
+const cartFilledDiv = document.getElementById("cart-filled");
+const deleteBtn = document.getElementById("delete-button");
+const itemAmountSpan = document.getElementById("item-amount");
+const priceSpan = document.getElementById("price");
+const totalPriceSpan = document.getElementById("total-price");
+const prevBtn = document.getElementById("prev-button");
+const nextBtn = document.getElementById("next-button");
+const productImage = document.getElementById("product-image");
+
+let itemAmount = Number(amountSpan.textContent);
+let currentItemAmount = 0;
+let price = Number(priceSpan.textContent.replace("$", ""));
+let totalPrice = Number(totalPriceSpan.textContent);
+
+const images = [
+    "./images/image-product-1.jpg",
+    "./images/image-product-2.jpg",
+    "./images/image-product-3.jpg",
+    "./images/image-product-4.jpg",
+];
+let currentImage = 0;
 
 sidebarBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("hidden");
-    overlayBlur.classList.add("overlay");
+    sidebar.classList.toggle("open");
+    overlayBlur.classList.toggle("overlay");
 });
 
 closeBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("hidden");
-    overlayBlur.classList.remove("overlay");
+    sidebar.classList.toggle("open");
+    overlayBlur.classList.toggle("overlay");
 });
 
 cartBtn.addEventListener("click", () => {
     cartPopup.classList.toggle("hidden");
+});
+
+prevBtn.addEventListener("click", () => {
+    if(currentImage <= 0) {
+        currentImage = 3;
+    } else {
+        currentImage -= 1;
+    }
+    productImage.src = images[currentImage];
+});
+
+nextBtn.addEventListener("click", () => {
+    if(currentImage >= 3) {
+        currentImage = 0;
+    } else {
+        currentImage += 1;
+    }
+    productImage.src = images[currentImage];
+});
+
+minusButton.addEventListener("click", () => {
+    if(itemAmount >= 1) {
+        itemAmount -= 1;
+        amountSpan.textContent = itemAmount;
+    }
+});
+
+addButton.addEventListener("click", () => {
+    itemAmount += 1;
+    amountSpan.textContent = itemAmount;
+});
+
+addToCartBtn.addEventListener("click", () => {
+    currentItemAmount += itemAmount;
+    
+    if(itemAmount >= 1) {
+        numberItemsCart.textContent = currentItemAmount;
+        itemAmountSpan.textContent = currentItemAmount;
+
+        totalPrice = price * currentItemAmount;
+        totalPriceSpan.textContent = `$${totalPrice.toFixed(2)}`;
+
+        numberItemsCart.classList.remove("hidden");
+        cartEmptyDiv.classList.add("hidden");
+        cartFilledDiv.classList.remove("hidden");
+    }
+
+    amountSpan.textContent = "0";
+    itemAmount = 0;
+});
+
+deleteBtn.addEventListener("click", () => {
+    if(currentItemAmount >= 1) {
+        currentItemAmount -= 1;
+
+        numberItemsCart.textContent = currentItemAmount;
+        itemAmountSpan.textContent = currentItemAmount;
+
+        totalPrice = price * currentItemAmount;
+        totalPriceSpan.textContent = `$${totalPrice.toFixed(2)}`;
+
+        if(currentItemAmount === 0) {
+            numberItemsCart.classList.add("hidden");
+            cartEmptyDiv.classList.remove("hidden");
+            cartFilledDiv.classList.add("hidden");
+        }
+    } 
 });
